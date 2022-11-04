31f35762cad460e5ed0126bdfc730d984a63d432
Chaejeong <sma07174@naver.com>
2022-11-04 23:27:34 +0900

    update_index

--- src/main/java/com/graduation/daily/controller/DiseaseController.java
+++ src/main/java/com/graduation/daily/controller/DiseaseController.java
@@ -15,4 +15,13 @@ public class DiseaseController {
 
     @GetMapping("/disease_turtleneck")
     public String disease_turtleneck() {return "disease/disease_turtleneck";}
+
+    @GetMapping("/disease_rcs")
+    public String disease_rcs() {return "disease/disease_rcs";}
+
+    @GetMapping("/disease_hand")
+    public String disease_hand(){return "disease/disease_hand";}
+
+    @GetMapping("/disease_halluxValgus")
+    public String disease_halluxV(){return "disease/disease_halluxValgus";}
 }
